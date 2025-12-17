37f721ea3f3f1e86570201e391b98fb4b8079606
flutteruts4_007 <nogasalsabilla@gmail.com>
2025-12-17 23:14:59 +0700

    utils

--- utils/security.go
+++ utils/security.go
@@ -17,12 +17,14 @@ func CheckPasswordHash(password, hash string) bool {
 }
 
 func GenerateJWT(userID, username, role string, permissions []string) (string, error) {
+	now := time.Now()
 	claims := jwt.MapClaims{
 		"user_id":     userID,
 		"username":    username,
 		"role":        role,
 		"permissions": permissions,
-		"exp":         time.Now().Add(time.Hour * 24).Unix(),
+		"iat":         now.Unix(),                     // Issued At
+		"exp":         now.Add(time.Hour * 24).Unix(), // Expires At
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
@@ -66,4 +68,52 @@ func ValidateToken(tokenString string) (jwt.MapClaims, error) {
 	return claims, nil
 }
 
+// GetTokenExpiration mengambil expiration time dari JWT token
+func GetTokenExpiration(tokenString string) (time.Time, error) {
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid signing method")
+		}
+		return JWTSecretKey, nil
+	})
 
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return time.Time{}, errors.New("invalid token claims")
+	}
+
+	if exp, ok := claims["exp"].(float64); ok {
+		return time.Unix(int64(exp), 0), nil
+	}
+
+	return time.Time{}, errors.New("expiration not found in token")
+}
+
+// GetTokenIssuedAt mengambil issued at time dari JWT token
+func GetTokenIssuedAt(tokenString string) (time.Time, error) {
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, errors.New("invalid signing method")
+		}
+		return JWTSecretKey, nil
+	})
+
+	if err != nil {
+		return time.Time{}, err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return time.Time{}, errors.New("invalid token claims")
+	}
+
+	if iat, ok := claims["iat"].(float64); ok {
+		return time.Unix(int64(iat), 0), nil
+	}
+
+	return time.Time{}, errors.New("issued at not found in token")
+}
