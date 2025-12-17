ee8eb9821fdab9933005d47e1e1bb7093f2fda18
flutteruts4_007 <nogasalsabilla@gmail.com>
2025-12-17 23:07:20 +0700

    student

--- app/model/Postgresql/Student.go
+++ app/model/Postgresql/Student.go
@@ -17,17 +17,17 @@ type Student struct {
 }
 
 type StudentWithUser struct {
-	ID            uuid.UUID ⁠ json:"id" ⁠
-	UserID        uuid.UUID ⁠ json:"user_id" ⁠
-	StudentID     string    ⁠ json:"student_id" ⁠
-	Program_Study string    ⁠ json:"program_study" ⁠
-	Academic_Year string    ⁠ json:"academic_year" ⁠
-	AdvisorID     uuid.UUID ⁠ json:"advisor_id" ⁠
-	Created_at    time.Time ⁠ json:"created_at" ⁠
+	ID            uuid.UUID `json:"id"`
+	UserID        uuid.UUID `json:"user_id"`
+	StudentID     string    `json:"student_id"`
+	Program_Study string    `json:"program_study"`
+	Academic_Year string    `json:"academic_year"`
+	AdvisorID     uuid.UUID `json:"advisor_id"`
+	Created_at    time.Time `json:"created_at"`
 	// User info
-	Username string ⁠ json:"username" ⁠
-	FullName string ⁠ json:"full_name" ⁠
-	Email    string ⁠ json:"email" ⁠
+	Username string `json:"username"`
+	FullName string `json:"full_name"`
+	Email    string `json:"email"`
 	// Advisor info
-	AdvisorName string ⁠ json:"advisor_name,omitempty" ⁠
+	AdvisorName string `json:"advisor_name,omitempty"`
 }
